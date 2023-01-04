41c9e3f7584ac5882616c6e9eb50d56726b1dee7
Sezer Turkdal <sezer.turkdal@hotmail.com>
2023-01-04 22:18:44 +0300

    Initial Commit

---
+++ FavoriteBook/FavoriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBookApp.swift
+//  FavoriteBook
+//
+//  Created by Sezer on 4.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
